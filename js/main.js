29e111dd3e0b0865db291e7798c6b9884caa88ea
asif-jalil <62372647+asif-jalil@users.noreply.github.com>
2020-07-29 02:44:02 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+  "use strict";
+
+  $(".open-btn").click(function () {
+    $(".sidebar").css("left", "0")
+  })
+
+  $(" .close-btn").click(function () {
+    $(".sidebar").css("left", "-100px")
+  })
+
+
+
+
+
+
+})(jQuery);
